9914b6b81320a268ae027e456341450fdf945ea6
Desi Dimova <dimova@gmail.com>
2018-11-13 14:53:35 +0200

    Audit fix.

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,9 @@ const router = express.Router()
 router.get('/', (req, res) => {
   res.render('index')
 })
+//adding the customer page
+router.get('/customer', (req, res) => {
+  res.render('customer')
+})
 
 module.exports = router
